c15093ccce950db0237f24d2ddb4a530ce67c80e
Anastasia <maximcoke69@gmail.com>
2020-12-28 17:12:38 +0300

    Added DB for searching

--- search/admin.py
+++ search/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import PhraseVersion, SearchPhrase
+from .models import SearchPhrase
 
-admin.site.register(PhraseVersion)
+# admin.site.register(PhraseVersion)
 admin.site.register(SearchPhrase)
 
--- search/views.py
+++ search/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import PhraseVersion, SearchPhrase
+from .models import SearchPhrase
 
 from .model.model import Search
 
 
 def index(request):
     search_phrase = SearchPhrase.objects.all().count()
-    phrase_version = PhraseVersion.objects.all().count()
+    # phrase_version = PhraseVersion.objects.all().count()
     questions = []
     if request.GET.get('search'):
         search = request.GET.get('search')
         questions = Search.searching(search)
+        new_search_phrase = SearchPhrase(search_phrase=search, answers=questions)
+        new_search_phrase.save()
+
         # if search in SearchPhrase.objects.all():
         #     questions.append(PhraseVersion.objects.all())
         # else:
@@ -21,6 +24,6 @@ def index(request):
     return render(
         request,
         'index.html',
-        context={'search_phrase': search_phrase, 'phrase_version': phrase_version, 'questions': questions}
+        context={'search_phrase': search_phrase, 'questions': questions}
     )
 
